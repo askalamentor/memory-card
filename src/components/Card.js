5d66cabf1dd5d09b1495bc57d3863b7ea63d3d97
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-01-30 17:17:17 +0300

    Create card component and set initial structure

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+export default function Card() {
+  return (
+    <div className="card-container">
+      <img src="#" className="card-img" alt="basketball player" />
+      <h3 className="card-text">Card text</h3>
+    </div>
+  );
+}
