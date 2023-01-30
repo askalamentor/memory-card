f5c64b4ad5c7a7e0faba436c3c39e633b4b14b75
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-01-30 18:08:37 +0300

    Create Player object

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+export const Players = {
+  array: [
+    { name: 'Damian Lillard', selected: false },
+    { name: 'Donovan Mitchell', selected: false },
+    { name: 'Giannis Antetokounmpo', selected: false },
+    { name: 'Ja Morant', selected: false },
+    { name: 'Jayson Tatum', selected: false },
+    { name: 'Jimmy Butler', selected: false },
+    { name: 'Joel Embiid', selected: false },
+    { name: 'Kevin Durant', selected: false },
+    { name: 'Kyrie Irving', selected: false },
+    { name: 'Lebron James', selected: false },
+    { name: 'Luka Doncic', selected: false },
+    { name: 'Nikola Jokic', selected: false },
+  ],
+};
