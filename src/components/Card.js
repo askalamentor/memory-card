90b175e05c3b2be199babe13f219d7cd869d7218
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-01-31 00:01:55 +0300

    Create static card overview

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,18 @@
-export default function Card() {
-  return (
-    <div className="card-container">
-      <img src="#" className="card-img" alt="basketball player" />
-      <h3 className="card-text">Card text</h3>
-    </div>
-  );
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div className="card-container" key={this.props.id}>
+        <img
+          src={require(`../images/${this.props.firstName.toLowerCase()}-${this.props.lastName.toLowerCase()}.png`)}
+          className="card-img"
+          alt="basketball player"
+        />
+        <h3 className="card-text">
+          {this.props.firstName} {this.props.lastName}
+        </h3>
+      </div>
+    );
+  }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import '../styles/Main.css';
+import { Players } from '../utils';
+import Card from './Card';
+import uniqid from 'uniqid';
 
 export default function Main() {
-  return <div>Main</div>;
+  const [players, setPlayers] = useState(Players);
+
+  return (
+    <div>
+      {players.array.map((player) => (
+        <Card
+          firstName={player.firstName}
+          lastName={player.lastName}
+          key={uniqid()}
+        />
+      ))}
+    </div>
+  );
 }
