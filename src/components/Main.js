6bf538b9bdde64c488dd5598a08fe12c20c9912e
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-01-31 20:51:33 +0300

    Create shuffleArray method to randomize card view

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,15 +5,37 @@ import Card from './Card';
 import uniqid from 'uniqid';
 
 export default function Main() {
-  const [players, setPlayers] = useState(Players);
+  const [players, setPlayers] = useState(Players.array);
+  const [randomArr, setRandomArr] = useState([]);
+
+  function handleCardClick(props) {
+    /*  if (!event.target.selected) {
+      setPlayers((event.target.selected = true));
+    } */
+    console.log(props.firstName);
+  }
+
+  useEffect(() => {
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      setRandomArr(array);
+    }
+
+    shuffleArray(players);
+  });
 
   return (
     <div className="Main">
-      {players.array.map((player) => (
+      {randomArr.map((player) => (
         <Card
           firstName={player.firstName}
           lastName={player.lastName}
           key={uniqid()}
+          selected={player.selected}
+          handleCardClick={handleCardClick}
         />
       ))}
     </div>
